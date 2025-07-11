e4f98053c669ab5fbbeedff509a01d6878dd3b79
hardikbeladiya <beladiyahardik7@gmail.com>
2025-07-11 19:04:49 +0530

    Support a way to dogfood CDN sdk

--- components/ApiKeyModal/index.tsx
+++ components/ApiKeyModal/index.tsx
@@ -1,14 +1,18 @@
 import { axiosWithApiKey } from '@/utils'
 import { getErrorCodeFromAxiosError } from '@/utils/getErrorCodeFromAxiosError'
-import { Alert, TextField } from '@mui/material'
+import { Alert } from '@mui/material'
 import Modal from '@mui/material/Modal'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useAsync, useAsyncFn } from 'react-use'
 import { saveApiKey } from '../../store/apiKeySlice'
+import { updateUseCdnSdk } from '../../store/kanmonConnectSlice'
 import Button from '../shared/Button'
 import { genericErrorMessage } from '@/utils/constants'
+import { Form, Formik } from 'formik'
+import * as Yup from 'yup'
+import FormikCheckboxInput from '@/components/shared/FormikCheckBoxField'
+import FormikTextInput from '@/components/shared/FormikTextField'
 
 const getSaveApiKeyErrorMessage = (error: any) => {
   if (!error) return null
@@ -20,16 +24,26 @@ const getSaveApiKeyErrorMessage = (error: any) => {
   return genericErrorMessage
 }
 
+export interface FormValues {
+  apiKey: string
+  useCdnSdk: boolean
+}
+
+const apiKeyValidationSchema = Yup.object().shape({
+  apiKey: Yup.string().trim().required('Api Key is required'),
+})
+
 const ApiKeyModal = ({ open }: any) => {
   const dispatch = useDispatch()
   const { query, pathname, replace, isReady } = useRouter()
-  const [localApiKey, setLocalApiKey] = useState('')
 
   const [{ loading: saveApiKeyLoading, error: saveApiKeyError }, saveApiKeyFn] =
-    useAsyncFn(async (apiKey: string) => {
+    useAsyncFn(async (values: FormValues) => {
+      const { apiKey, useCdnSdk } = values
       await axiosWithApiKey(apiKey).get('/api/test_api_key')
 
       dispatch(saveApiKey({ apiKey }))
+      dispatch(updateUseCdnSdk({ useCdnSdk }))
     })
 
   const error = getSaveApiKeyErrorMessage(saveApiKeyError)
@@ -44,7 +58,7 @@ const ApiKeyModal = ({ open }: any) => {
 
       if (queryApiKey) {
         try {
-          await saveApiKeyFn(queryApiKey)
+          await saveApiKeyFn({ apiKey: queryApiKey, useCdnSdk: false })
         } finally {
           // Remove query params after saving them
           replace(
@@ -60,50 +74,65 @@ const ApiKeyModal = ({ open }: any) => {
 
       return true
     },
-    [query?.kanmonApiKey],
+    [query?.kanmonApiKey, query?.useCdnSdk],
   )
 
   // Prevent flickering when api key is passed through query param
   if (!doneValidatedApiKeyFromQueryParam) return null
+
+  const initialValues: FormValues = {
+    apiKey: '',
+    useCdnSdk: false,
+  }
 
   return (
     <Modal open={open}>
       <>
         <div className="top-1/2 left-1/2 absolute transform -translate-x-1/2 -translate-y-1/2 bg-white text-center rounded w-[450px] md:w-[550px]">
           <div className="p-12">
-            <div className="mt-4">
-              <h1 className="text-xl font-semibold mb-8">
-                Access the Demo Platform with your API Key
-              </h1>
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault()
-                  saveApiKeyFn(localApiKey)
-                }}
-              >
-                <div className="mb-8">
-                  <TextField
-                    variant="outlined"
-                    value={localApiKey}
-                    placeholder={'Set Api Key'}
-                    fullWidth
-                    onChange={(e) => {
-                      setLocalApiKey(e.target.value)
-                    }}
-                  />
-                </div>
+            <Formik
+              onSubmit={async (values) => {
+                await saveApiKeyFn(values)
+              }}
+              initialValues={initialValues}
+              validationSchema={apiKeyValidationSchema}
+              validateOnMount={true}
+            >
+              {({ isValid, handleSubmit, isSubmitting }) => {
+                return (
+                  <div className="mt-4">
+                    <h1 className="text-xl font-semibold mb-8">
+                      Access the Demo Platform with your API Key
+                    </h1>
+                    <Form>
+                      <div className="mb-4">
+                        <FormikTextInput<FormValues>
+                          fieldName="apiKey"
+                          placeholder={'Set Api Key'}
+                        />
+                      </div>
+                      <div className="col-span-6">
+                        <FormikCheckboxInput<FormValues>
+                          fieldName="useCdnSdk"
+                          label="Use CDN SDK"
+                          containerClass="flex items-center mb-4"
+                        />
+                      </div>
 
-                <Button
-                  fullWidth
-                  variant="contained"
-                  disabled={!localApiKey || saveApiKeyLoading}
-                  color="primary"
-                  type="submit"
-                >
-                  Start new demo
-                </Button>
-              </form>
-            </div>
+                      <Button
+                        fullWidth
+                        variant="contained"
+                        disabled={!isValid || isSubmitting || saveApiKeyLoading}
+                        onClick={() => handleSubmit()}
+                        color="primary"
+                      >
+                        Start new demo
+                      </Button>
+                    </Form>
+                  </div>
+                )
+              }}
+            </Formik>
             {error && (
               <div className="my-4 text-left">
                 <Alert severity="error">{error}</Alert>
