3f992f6297ee99a69ddeef9637d08ee43497ea8e
hardikbeladiya <beladiyahardik7@gmail.com>
2025-07-16 10:28:31 +0530

    Allow KanmonOS users to be added to multiple Auth0 Orgs

--- components/wrappers.tsx
+++ components/wrappers.tsx
@@ -15,8 +15,10 @@ import { useDispatch, useSelector, useStore } from 'react-redux'
 import { useAsync } from 'react-use'
 import { PersistGate } from 'redux-persist/integration/react'
 import KanmonConnectContextProvider from '../hooks/KanmonConnectContext'
-import { getApiKeyState } from '../store/apiKeySlice'
+import { getApiKeyState, saveApiKey } from '../store/apiKeySlice'
 import ApiKeyModal from './ApiKeyModal'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 interface Props {
   children?: React.ReactNode
@@ -36,8 +38,19 @@ export const TempReduxWrapper: React.FC<Props> = ({ children }) => {
 
 export const TempAuthWrapper: React.FC<Props> = ({ children }) => {
   const { userId } = useSelector(getAuthState)
-  const { apiKey } = useSelector(getApiKeyState)
+  const { apiKey: storeApiKey } = useSelector(getApiKeyState)
   const { dispatch } = useStore()
+  const { query, isReady } = useRouter()
+
+  const queryApiKey = query?.kanmonApiKey as string | undefined
+
+  const apiKey = queryApiKey || storeApiKey
+  
+  useEffect(() => {
+    if(isReady && !storeApiKey && apiKey) {
+      dispatch(saveApiKey({ apiKey }))
+    }
+  }, [isReady, storeApiKey, apiKey, dispatch])  
 
   const { loading: validatingApiKey } = useAsync(async () => {
     try {
@@ -60,6 +73,9 @@ export const TempAuthWrapper: React.FC<Props> = ({ children }) => {
         <ApiKeyModal open />
       </div>
     )
+  }
+  if (!isReady) {
+    return null
   }
 
   return <div>{userId ? children : <BusinessSelectionModalV2 open />}</div>
